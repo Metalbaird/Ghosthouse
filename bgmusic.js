711d562fa600442210c97c64fc8df205e3f1df2c
Metalbaird <Calumbaird43@hotmail.com>
2024-04-22 11:47:05 +0100

    Testing music

---
+++ bgmusic.js
@@ -0,0 +1,7 @@
+// Get the audio element
+const bgMusic = document.getElementById('bgMusic');
+
+// Play the background music when the page loads
+window.addEventListener('load', function() {
+    bgMusic.play();
+});
